028fcd94e31c1007261a2a6933d1c3d00c134448
Sneha Vishwakarma <88725034+snehavish595@users.noreply.github.com>
2026-06-11 15:40:05 +0530

    redesign footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="py-4 text-center bg-[#202124] text-bold text-white ">
-      {" "}
-      &copy; 2024 Sneha Vishwakarma. all right reserved
-    </div>
+    <footer className="bg-[#f2f2f2] border-t border-[#dadce0] text-sm text-[#70757a]">
+      <div className="container mx-auto px-6 py-3 flex flex-col sm:flex-row items-center justify-between gap-2 text-xs">
+        <div>
+          &copy; {new Date().getFullYear()} Sneha Vishwakarma. All rights reserved.
+        </div>
+       
+      </div>
+    </footer>
   );
 };
 
